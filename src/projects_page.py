86055843be7b671202539d57f6c191228a903a60
lior42 <liorlander@gmail.com>
2023-11-12 22:44:09 +0200

    try fix git logo in project, attempt 2

--- src/projects_page.py
+++ src/projects_page.py
@@ -3,8 +3,11 @@ from my_templates import MyTemplates
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from pydantic import NonNegativeInt
+from pathlib import Path
 
 router = APIRouter(prefix="/projects")
+
+__STATIC_DIR = Path("static")
 
 
 @router.get("/", response_class=HTMLResponse)
@@ -22,6 +25,12 @@ async def projects_page_main(
 
     all_projects = await basic_query.to_list(None)
 
+    github_svg = ""
+    github_logo_f = __STATIC_DIR / "images" / "github-logo" / "m-black.svg"
+
+    github_svg = github_logo_f.read_text()
+
     return templates.TemplateResponse(
-        "pages/projects.j2", {"request": request, "projects_list": all_projects}
+        "pages/projects.j2",
+        {"request": request, "projects_list": all_projects, "github_logo": github_svg},
     )
